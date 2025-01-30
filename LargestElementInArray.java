642556c9317d5f1126891c72bd7b9b40780fc9fb
Soumik Ghosh <soumikghosh09072004@gmail.com>
2025-01-30 22:52:32 +0530

    Largest Element in Array added

--- LargestElementInArray.java
+++ LargestElementInArray.java
@@ -1,3 +1,5 @@
+// Write a Java program to find the largest element in an integer array.
+
 public class LargestElementInArray {
     public static void main(String[] args) {
         int[] numbers = {12, 45, 7, 34, 89, 22}; 
