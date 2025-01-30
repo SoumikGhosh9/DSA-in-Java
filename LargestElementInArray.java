6702c77f111741b2910e01e568cf0efe2291a8a6
Soumik Ghosh <soumikghosh09072004@gmail.com>
2025-01-30 22:51:29 +0530

    Largest Element in Array added

---
+++ LargestElementInArray.java
@@ -0,0 +1,15 @@
+public class LargestElementInArray {
+    public static void main(String[] args) {
+        int[] numbers = {12, 45, 7, 34, 89, 22}; 
+
+        int max = numbers[0];  
+
+        for (int number : numbers) {
+            if (number > max) {
+                max = number;
+            }
+        }
+
+        System.out.println("The largest element is: " + max);
+    }
+}
