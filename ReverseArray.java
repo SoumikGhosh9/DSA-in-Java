f2fe5fc157f964637880292fff7cd0e3647854f6
Soumik Ghosh <soumikghosh09072004@gmail.com>
2025-02-06 20:17:22 +0530

    Reverse of an Array element

---
+++ ReverseArray.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+public class ReverseArray{
+        static void reverse(int arr[]){
+            int left=0, right = arr.length-1;
+            while(left < right){
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        public static void main(String[] args){
+            int arr[] = {5,4,3,2,1};
+            reverse(arr);
+            System.out.print(Arrays.toString(arr));
+        }
+    }
